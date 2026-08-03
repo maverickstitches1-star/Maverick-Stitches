7ad5258480adbebde2fc8c9a0920f3de3985534b
maverickstitches1-star <maverickstitches1@gmail.com>
2026-08-03 21:48:26 +0100

    move main.js to the js folder

--- js/main.js
+++ js/main.js
@@ -509,3 +509,48 @@ window.addEventListener("load", () => {
     }
 
 });
+
+/*==================================
+LIGHT & DARK THEME
+==================================*/
+
+const themeToggle = document.getElementById("themeToggle");
+
+const body = document.body;
+
+const icon = themeToggle.querySelector("i");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "light"){
+
+    body.classList.add("light-theme");
+
+    icon.classList.remove("fa-moon");
+
+    icon.classList.add("fa-sun");
+
+}
+
+themeToggle.addEventListener("click", ()=>{
+
+    body.classList.toggle("light-theme");
+
+    if(body.classList.contains("light-theme")){
+
+        icon.classList.remove("fa-moon");
+
+        icon.classList.add("fa-sun");
+
+        localStorage.setItem("theme","light");
+
+    }else{
+
+        icon.classList.remove("fa-sun");
+
+        icon.classList.add("fa-moon");
+
+        localStorage.setItem("theme","dark");
+
+    }
+
+});
